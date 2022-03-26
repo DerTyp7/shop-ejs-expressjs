a69b69d0573d272fad84bed7d926d417cf2cbb21
j.mei7 <janis.meister87@gmail.com>
2022-03-26 16:01:22 +0100

    Update onlineshop.sql

--- onlineshop.sql
+++ onlineshop.sql
@@ -26,13 +26,14 @@ CREATE TABLE `addresses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `street` varchar(60) NOT NULL,
   `housenumber` int(11) NOT NULL,
-  `postcode` varchar(30) NOT NULL,
-  `city` varchar(60) NOT NULL,
   `country` varchar(60) NOT NULL,
   `userId` int(11) NOT NULL,
+  `cityId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `addresses_ibfk_1` (`userId`),
-  CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  KEY `cityId` (`cityId`),
+  CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `addresses_ibfk_2` FOREIGN KEY (`cityId`) REFERENCES `cities` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -42,7 +43,6 @@ CREATE TABLE `addresses` (
 
 LOCK TABLES `addresses` WRITE;
 /*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
-INSERT INTO `addresses` VALUES (1,'street',1,'postcode','city','country',18);
 /*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,6 +72,31 @@ INSERT INTO `categories` VALUES (3,'Bauwaren'),(1,'Elektrowaren'),(4,'Television
 UNLOCK TABLES;
 
 --
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `postcode` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES (1,'Bremerhaven','27574'),(2,'Bremerhaven','27574');
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `order_products`
 --
 
@@ -89,7 +114,7 @@ CREATE TABLE `order_products` (
   KEY `orderId` (`orderId`),
   CONSTRAINT `order_products_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`) ON DELETE CASCADE,
   CONSTRAINT `order_products_ibfk_2` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +123,7 @@ CREATE TABLE `order_products` (
 
 LOCK TABLES `order_products` WRITE;
 /*!40000 ALTER TABLE `order_products` DISABLE KEYS */;
-INSERT INTO `order_products` VALUES (6,1.50,50,1,1);
+INSERT INTO `order_products` VALUES (6,1.50,50,1,1),(7,2.00,0,2,9),(8,2.00,37,2,10),(9,2.00,22,2,11),(10,2.00,22,2,12);
 /*!40000 ALTER TABLE `order_products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,7 +140,7 @@ CREATE TABLE `order_status` (
   `trackingnumber` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `trackingnumber` (`trackingnumber`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,7 +149,7 @@ CREATE TABLE `order_status` (
 
 LOCK TABLES `order_status` WRITE;
 /*!40000 ALTER TABLE `order_status` DISABLE KEYS */;
-INSERT INTO `order_status` VALUES (5,0,'undefined'),(8,0,'tasdadse'),(9,0,'tasddadse');
+INSERT INTO `order_status` VALUES (5,0,'undefined'),(8,0,'tasdadse'),(9,0,'tasddadse'),(11,0,'e19f1b04-8eb8-42ad-b0e0-ca0de80565fa'),(12,0,'1f96e5bc-5e21-4dd5-b977-e49953b2ffd4'),(13,0,'8775feb6-f561-4ef0-8788-77e4af006726'),(14,0,'6ffd662f-547a-484b-b84d-6c1cd0e6135c'),(15,0,'3e0c73e6-2243-401a-9ef4-88db0903d600'),(16,0,'76bec49c-f51a-4690-aed3-2405ff7fb626'),(17,0,'d6343eaf-719d-4e66-821f-f2c70ee4aa08'),(18,0,'b291d096-4f4c-4fc4-8c4a-3684f1ad4c93'),(19,0,'e5f7c5c0-273c-4c53-97c9-4656f1e57262'),(20,0,'8ca728ff-e52d-4199-95f1-681b032aedba'),(21,0,'0ba2e8f3-475b-4645-acac-d4a188b762aa'),(22,0,'d4ec8293-7007-4274-bdd3-965a6bef36cd'),(23,0,'2e639e69-7397-40a9-aa46-12d689c2306b'),(24,0,'d265236b-78d7-4324-b35c-a0ea81d7984b'),(25,0,'23baa6d0-e5cb-4a88-a545-9512029dc774'),(26,0,'08361df5-1155-42dc-9748-2b8ab07fe068'),(27,0,'20a59258-4af8-429c-9619-cc1da7bf9c53');
 /*!40000 ALTER TABLE `order_status` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +169,7 @@ CREATE TABLE `orders` (
   KEY `orders_ibfk_2` (`order_statusId`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`order_statusId`) REFERENCES `order_status` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,7 +178,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,8,18),(2,9,18),(3,9,18);
+INSERT INTO `orders` VALUES (1,8,18),(2,9,18),(3,9,18),(4,14,27),(5,15,27),(6,16,27),(7,17,27),(8,18,27),(9,19,27),(10,20,27),(11,26,27),(12,27,27);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,7 +212,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'LED Leiste mit RGBW',3.56,'Mit warmweiß funktion',2,2,1,1),(2,'Nagel ',2.00,'Gut zum nageln',200,2,1,3),(3,'Glühbirne x35 10000000 Watt',1.60,'Glühbirne mit viel Watt',2,2,1,1),(4,'LG TV 500x QHD',130.99,'Richtig guter TV mit perfekter Qualität',27,3,1,4);
+INSERT INTO `products` VALUES (1,'LED Leiste mit RGBW',3.56,'Mit warmweiß funktion',2,2,1,1),(2,'Nagel ',2.00,'Gut zum nageln',119,2,1,3),(3,'Glühbirne x35 10000000 Watt',1.60,'Glühbirne mit viel Watt',2,2,1,1),(4,'LG TV 500x QHD',130.99,'Richtig guter TV mit perfekter Qualität',27,3,1,4);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -265,7 +290,7 @@ CREATE TABLE `userinfos` (
   PRIMARY KEY (`id`),
   KEY `userinfos_ibfk_1` (`userId`),
   CONSTRAINT `userinfos_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -274,6 +299,7 @@ CREATE TABLE `userinfos` (
 
 LOCK TABLES `userinfos` WRITE;
 /*!40000 ALTER TABLE `userinfos` DISABLE KEYS */;
+INSERT INTO `userinfos` VALUES (6,'Janis','Meister','testuser',27);
 /*!40000 ALTER TABLE `userinfos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -293,7 +319,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -302,7 +328,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (18,'dertyp','address@email.com','password',0),(19,'janis','janis.meister87@gmail.com','$2a$10$uRt2KBcBcIHVD24XPdCIXeXfJmE6k/78CuXnSi0ukTEA9m0qfyKA6',0),(20,'janis2','jadnis.meister87@gmail.com','$2a$10$QuAII9wQsbKeFMVtJON/r.ke.jKGbUBBd24hUQWg65nKcVNDjJiGG',1);
+INSERT INTO `users` VALUES (18,'dertyp','address@email.com','password',0),(19,'janis','janis.meister87@gmail.com','$2a$10$uRt2KBcBcIHVD24XPdCIXeXfJmE6k/78CuXnSi0ukTEA9m0qfyKA6',0),(20,'janis2','jadnis.meister87@gmail.com','$2a$10$QuAII9wQsbKeFMVtJON/r.ke.jKGbUBBd24hUQWg65nKcVNDjJiGG',1),(27,'testuser','janis.mseister87@gmail.com','$2a$10$Xk2t83/CuR2iF5q/gOaqiOxg1TT155ML7fJGjTYSL84VPiZPMGZDe',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -315,4 +341,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-26 11:24:57
+-- Dump completed on 2022-03-26 16:01:18
